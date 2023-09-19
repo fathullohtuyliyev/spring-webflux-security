566f1eb3804c2066d20a70a744a3ac6909225d64
Fathullo.Tuyliyev <asbt2223>
2023-09-19 18:48:55 +0500

    created

---
+++ src/test/java/uzb/tuyliyev/springwebfluxsecurity/SpringWebfluxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package uzb.tuyliyev.springwebfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
